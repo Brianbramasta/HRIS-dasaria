b41df00d2f1fd9a971ff8e214d9044aafa7a2718
Brian Bramasta <brianaldybramasta@gmail.com>
2025-12-18 16:11:26 +0700

    fix(src): delete all file

--- src/features/employee/components/employee-date/tab/StoryPayroll.tsx
+++
@@ -1,9 +0,0 @@
-import ExpandCard from '@/features/structure-and-organize/components/card/ExpandCard';
-interface Props { data: import('../../../types/Employee').Karyawan; isEditable: boolean }
-export default function StoryPayrollTab({ isEditable }: Props) {
-  return (
-    <ExpandCard title={isEditable ? 'Story Payroll (Edit)' : 'Story Payroll'} withHeaderDivider>
-      <div className="text-sm text-gray-500">Belum ada histori payroll.</div>
-    </ExpandCard>
-  );
-}
