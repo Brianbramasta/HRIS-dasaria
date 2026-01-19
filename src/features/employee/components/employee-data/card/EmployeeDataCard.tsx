c300a6f514dd0107e6f3c1c50d9d7a31f08008ab
Brian Bramasta <brianaldybramasta@gmail.com>
2026-01-19 11:30:45 +0700

    fix(employee-data-card): uncomment and enable edit button visibility.

--- src/features/employee/components/employee-data/card/EmployeeDataCard.tsx
+++ src/features/employee/components/employee-data/card/EmployeeDataCard.tsx
@@ -18,7 +18,7 @@ export default function EmployeeDataCard({ data, employeeId }: Props & { employe
     closeModal,
     initialForm,
     isComplete,
-    // isEditHidden,
+    isEditHidden,
     handleSubmit,
   } = useEmployeeDataCard(data, employeeId);
 
@@ -91,13 +91,13 @@ export default function EmployeeDataCard({ data, employeeId }: Props & { employe
         </div>
       </div>
 
-      {/* {!isEditHidden && ( */}
+      {!isEditHidden && (
         <div className="mt-4 flex justify-end">
           <Button variant="primary" size="sm" onClick={openModal} className='w-full md:w-auto flex items-center justify-center'>
             <Edit2 size={16} className="mr-2" /> Edit
           </Button>
         </div>
-      {/* )}  */}
+      )}  
       <EmployeeDataModal
         isOpen={isOpen}
         initialData={initialForm}
