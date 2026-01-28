a8a49957c60f56baa80ab8d34be79be2dd54653e
Brian Bramasta <brianaldybramasta@gmail.com>
2026-01-28 16:46:03 +0700

    fix(employee): use nullish coalescing for avatar fallback.

--- src/features/employee/pages/employee-data/detail/DetailPage.tsx
+++ src/features/employee/pages/employee-data/detail/DetailPage.tsx
@@ -43,7 +43,7 @@ export default function DetailKaryawanPage() {
         <div className="flex flex-col md:flex-row items-center justify-between gap-4">
           <div className="flex flex-col md:flex-row items-center gap-3">
             <img
-              src={formatUrlFile(detail?.Personal_Data?.avatar )?? 'https://api.dicebear.com/7.x/avataaars/svg?seed=${id}'}
+              src={formatUrlFile(detail?.Personal_Data?.avatar ?? 'https://api.dicebear.com/7.x/avataaars/svg?seed=${id}')}
               alt="Employee Avatar"
               className="h-12 w-12 rounded-full"
             />
