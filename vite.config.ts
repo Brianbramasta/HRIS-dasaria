f06b65721bbd16344d8f4593df480b3901f277e7
Brian Bramasta <brianaldybramasta@gmail.com>
2026-02-10 15:53:06 +0700

    chore: update vite config for ngrok and adjust UI text.

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,12 @@ export default defineConfig(({ mode }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    server: {
+    server: { 
+      // nanti di production hapus ini
+      host: true,
+      allowedHosts: [
+        'stephenie-spriggy-lacey.ngrok-free.dev'
+      ],
       watch: {
         // Ignore changes to db.json and server.js to prevent dev reloads
         // when json-server writes updates after PATCH/POST.
