cc1deaa45958041875657da599894a5d96d82dd5
Brianbramasta <Brianaldybramasta@gmail.com>
2026-02-17 15:04:24 +0700

    fix(payroll):import yang ga kepakai di hapus

--- src/features/payroll/pages/payroll-period/detail/detailPayrollAEPage.tsx
+++ src/features/payroll/pages/payroll-period/detail/detailPayrollAEPage.tsx
@@ -3,7 +3,6 @@ import { useMemo } from "react";
 import { useParams } from "react-router";
 import DetailPayrollContent, { SectionConfig } from "@/features/payroll/components/layouts/LayoutDetail";
 import TambahTunjanganTidakTetapModalAE from "@/features/payroll/components/modals/detail-payroll/ae/AddNonRecurringAllowanceModal";
-import TambahPotonganTidakTetapModalAE from "@/features/payroll/components/modals/detail-payroll/ae/AddNonRecurringDeductionModal";
 import EditInformationEmployeeModal from "@/features/payroll/components/modals/detail-payroll/non-ae/EditInformationEmployeeModal";
 
 // Dokumentasi: Komponen halaman AE yang menyusun config untuk layout dinamis
