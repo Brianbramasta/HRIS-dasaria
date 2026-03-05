9a83c6594daad6d159dfe379516690486d9835bb
Brian Bramasta <brianaldybramasta@gmail.com>
2026-03-05 11:12:33 +0700

    feat(auth): add customizable admin contact with WhatsApp integration.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -96,6 +96,7 @@ import SelectServicePage from "../features/auth/pages/SelectServicePage";
 import ProtectedOutlet from "./ProtectedOutlet";
 import FormKasbonPage from "@/features/payroll/pages/cash-advance/form-cash-advance/FormCashAdvancePage";
 import TemporaryApiPage from "@/pages/OtherPage/TemporaryApiPage";
+import SetContactAdmin from "@/pages/OtherPage/SetContactAdmin";
 import NotFound from "@/pages/OtherPage/NotFound";
 
 function PelanggaranRoute() {
@@ -245,6 +246,7 @@ export default function AppRoutes() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/reset-password" element={<ResetPasswordPage />} />
+      <Route path="/set-contact-admin" element={<SetContactAdmin />} />
       <Route path="/temporary-api" element={<TemporaryApiPage />} />
 
 
