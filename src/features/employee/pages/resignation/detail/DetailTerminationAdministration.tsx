6eb3bff9bbbf2c58bb15d32429a8f7905b0a4110
Brian Bramasta <brianaldybramasta@gmail.com>
2026-04-20 15:26:18 +0700

    refactor: remove unused useEffect import from detail page.

--- src/features/employee/pages/resignation/detail/DetailTerminationAdministration.tsx
+++ src/features/employee/pages/resignation/detail/DetailTerminationAdministration.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Button from '../../../../../components/ui/button/Button';
 import Label from '../../../../../components/form/Label';
